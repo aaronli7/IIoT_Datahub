6b9e87e15ed38c5ed4fff158a7beb9b89ddfe89b
aaronli-uga <ql61608@uga.edu>
2023-02-08 09:36:38 -0500

    update

--- data_acquisition/PMU_upload.py
+++ data_acquisition/PMU_upload.py
@@ -2,8 +2,8 @@
 Author: Qi7
 Date: 2023-02-07 20:25:04
 LastEditors: aaronli-uga ql61608@uga.edu
-LastEditTime: 2023-02-07 22:18:32
-Description: 
+LastEditTime: 2023-02-08 09:31:22
+Description: uploading data to the lab influxdb server
 '''
 import socket
 import json
@@ -17,6 +17,7 @@ import warnings
 import pandas as pd
 from waveform_to_PMU import feature_extract
 import numpy as np
+import influxdb_client
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS, ASYNCHRONOUS
 
@@ -30,11 +31,12 @@ def df_int_to_float(df):
     return df
 
 #influxdb config
-token = "abc"
+token = ""
 org = "lab711"
 bucket = "testbed"
 url = "sensorwebdata.engr.uga.edu:8086"
 measurement = "NI_Waveform"
+location = "lab711"
 
 client = influxdb_client.InfluxDBClient(
     url=url,
@@ -69,32 +71,31 @@ while True:
     # ts = tz_NY.localize(ts)
     currentTime = datetime.datetime.now(tz_NY)
     timestamp = int(currentTime.timestamp() * 1000000000)
-    p = influxdb_client.Point(measurement)
-                        .tag("location", location)
-                        .field("sensor1_DC_mag", features[0][0])
-                        .field("sensor1_DC_freq", features[1][0])
-                        .field("sensor1_DC_angle", features[2][0])
-                        .field("sensor1_DC_thd", features[3][0])
-                        .field("sensor1_AC_mag", features[4][0])
-                        .field("sensor1_AC_freq", features[5][0])
-                        .field("sensor1_AC_angle", features[6][0])
-                        .field("sensor1_AC_thd", features[7][0])
-                        .field("sensor2_DC_mag", features[8][0])
-                        .field("sensor2_DC_freq", features[9][0])
-                        .field("sensor2_DC_angle", features[10][0])
-                        .field("sensor2_DC_thd", features[11]][0])
-                        .field("sensor2_AC_mag", features[12][0])
-                        .field("sensor2_AC_freq", features[13][0])
-                        .field("sensor2_AC_angle", features[14][0])
-                        .field("sensor2_AC_thd", features[15][0])
-                        .field("sensor3_DC_mag", features[16][0])
-                        .field("sensor3_DC_freq", features[17][0])
-                        .field("sensor3_DC_angle", features[18][0])
-                        .field("sensor3_DC_thd", features[19][0])
-                        .field("sensor3_AC_mag", features[20][0])
-                        .field("sensor3_AC_freq", features[21][0])
-                        .field("sensor3_AC_angle", features[22][0])
-                        .field("sensor3_AC_thd", features[23][0])
+    p = influxdb_client.Point(measurement) \
+                        .tag("location", location) \
+                        .field("sensor1_DC_mag", features[0][0]) \
+                        .field("sensor1_DC_freq", features[1][0]) \
+                        .field("sensor1_DC_angle", features[2][0]) \
+                        .field("sensor1_DC_thd", features[3][0]) \
+                        .field("sensor1_AC_mag", features[4][0]) \
+                        .field("sensor1_AC_freq", features[5][0]) \
+                        .field("sensor1_AC_angle", features[6][0]) \
+                        .field("sensor1_AC_thd", features[7][0]) \
+                        .field("sensor2_DC_mag", features[8][0]) \
+                        .field("sensor2_DC_freq", features[9][0]) \
+                        .field("sensor2_DC_angle", features[10][0]) \
+                        .field("sensor2_DC_thd", features[11][0]) \
+                        .field("sensor2_AC_mag", features[12][0]) \
+                        .field("sensor2_AC_freq", features[13][0]) \
+                        .field("sensor2_AC_angle", features[14][0]) \
+                        .field("sensor2_AC_thd", features[15][0]) \
+                        .field("sensor3_DC_mag", features[16][0]) \
+                        .field("sensor3_DC_freq", features[17][0]) \
+                        .field("sensor3_DC_angle", features[18][0]) \
+                        .field("sensor3_DC_thd", features[19][0]) \
+                        .field("sensor3_AC_mag", features[20][0]) \
+                        .field("sensor3_AC_freq", features[21][0]) \
+                        .field("sensor3_AC_angle", features[22][0]) \
+                        .field("sensor3_AC_thd", features[23][0]) \
                         .time(timestamp)
-                        
     write_api.write(bucket=bucket, org=org, record=p)
