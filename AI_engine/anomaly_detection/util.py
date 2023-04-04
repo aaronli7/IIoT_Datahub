0618aa2bda9245ac53942d30dede0167d7fc0b1f
aaronli-uga <ql61608@uga.edu>
2023-04-04 13:31:34 -0400

    update

--- AI_engine/anomaly_detection/util.py
+++ AI_engine/anomaly_detection/util.py
@@ -2,7 +2,7 @@
 Author: Qi7
 Date: 2023-03-02 11:28:10
 LastEditors: aaronli-uga ql61608@uga.edu
-LastEditTime: 2023-03-02 14:26:22
+LastEditTime: 2023-03-21 17:38:18
 Description: 
 '''
 #!/usr/bin/env python3
@@ -10,7 +10,18 @@ import time
 # python API client for influx 2.x
 import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS, ASYNCHRONOUS
+import pytz
+import datetime
 
+def local_time_epoch(time, zone):
+    local_tz = pytz.timezone(zone)
+    localTime = datetime.strptime(time, "%Y-%m-%dT%H:%M:%S.%f")
+    local_dt = local_tz.localize(localTime, is_dst=None)
+    # utc_dt = local_dt.astimezone(pytz.utc)
+    epoch = local_dt.timestamp()
+    # print("epoch time:", epoch) # this is the epoch time in seconds, times 1000 will become epoch time in milliseconds
+    # print(type(epoch)) # float
+    return epoch 
 
 def write_influx2(influx, unit, table_name, data_name, data, start_timestamp, fs):
     bucket = influx['bucket']
