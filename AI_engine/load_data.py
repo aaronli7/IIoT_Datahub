a6c9cc50c20750481c9956db639b4591cc03e299
sjc230 <70530110+sjc230@users.noreply.github.com>
2023-06-27 15:46:58 -0400

    upate

--- AI_engine/load_data.py
+++ AI_engine/load_data.py
@@ -4,6 +4,10 @@ import pandas as pd
 from pandas import read_csv
 from pathlib import Path
 
-def load_data(data_path, data_filename):
-  data_file = np.load(data_path / data_filename)
+p = Path('.')
+datapath = p / "test_data/"
+
+def load(data_filename):
+  data_file = np.load(datapath / data_filename)
   return data_file
+
--- AI_engine/main.py
+++ AI_engine/main.py
@@ -25,5 +25,24 @@ from torchinfo import summary
 from sklearn.model_selection import train_test_split
 from torch.nn import functional as F
 
-p = Path('.')
-datapath = p / "test_data/"
+import load_data
+
+x = load_data.load('X.npy')
+y = load_data.load('y.npy')
+
+# normalization on input data x
+x = (x - x.mean(axis=0)) / x.std(axis=0)
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=42)
+
+#training_set = np.concatenate((X_train, y_train.reshape(-1,1)) ,axis=1)
+#test_set = np.concatenate((X_test, y_test.reshape(-1,1)) ,axis=1)
+
+X_train = np.transpose(X_train,(0,2,1))
+X_test = np.transpose(X_test,(0,2,1))
+
+#y_train = np.expand_dims(y_train,axis=1)
+#y_test = np.expand_dims(y_test,axis=1)
+
+training_set = waveformDataset(X_train, y_train)
+test_set = waveformDataset(X_test, y_test)
