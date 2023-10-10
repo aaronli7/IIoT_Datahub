81d82c1dc454fb8573968cd1b10d895bfb37832e
sjc230 <70530110+sjc230@users.noreply.github.com>
2023-10-09 21:08:06 -0400

    updates

--- AI_engine/website/views.py
+++ AI_engine/website/views.py
@@ -1,11 +1,19 @@
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 from .models import Note
 from . import db
 import json
+from pathlib import Path
+
+p = Path('.')
+datapath = p / "AI_engine/test_data/"
 
 views = Blueprint('views', __name__)
 
+UPLOAD_FOLDER = datapath
+ALLOWED_EXTENSIONS = {'npy'}
 
 @views.route('/', methods=['GET', 'POST'])
 @login_required
@@ -23,6 +31,29 @@ def home():
 
     return render_template("types.html", user=current_user)
 
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@views.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+    return
 
 @views.route('/delete-note', methods=['POST'])
 def delete_note():  
